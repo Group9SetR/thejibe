c3c0cf88d99fcf29461c4d6d6c54a2a73533b7a7
Vincent Lee <vincent.h.lee@outlook.com>
2017-05-10 12:52:20 -0700

    removed filter sub nav bar and moved to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.app')
+@section('dashboardnav')
+    <div class="secondnav" id="mySecondnav">
+        <div class="form-group">
+            <div class="col-sm-2">
+                <select class="form-control" id="client">
+                    <option>All Clients</option>
+                    <option>client 1</option>
+                    <option>client 2</option>
+                </select>
+            </div>
+            <div class="col-sm-2">
+                <select class="form-control" id="project">
+                    <option>All Projects</option>
+                    <option>project 1</option>
+                    <option>project 2</option>
+                </select>
+            </div>
+            <div class="col-sm-2">
+                <select class="form-control" id="priority">
+                    <option>All Priorities</option>
+                    <option>Priorities 1</option>
+                    <option>Priorities 2</option>
+                </select>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right ">
+                    {{ Form::open(['url'=>'/select-date']) }}
+                    <div class ="form-inline">
+                        {{ Form::date('start-date', '', ['id'=>'start-date', 'class'=>'form-control']) }}
+                        {{ Form::date('end-date', '', ['id'=>'end-date', 'class'=>'form-control']) }}
+                    </div>
+                    {{ Form::close() }}
 
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+
+@endsection
 @section('content')
     <section class="container">
         <div class="left-half">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,40 +46,7 @@
                     </div>
                 </div>
             </div>
-            <div class="secondnav" id="mySecondnav">
-                <div class="form-group">
-                    <div class="col-sm-2">
-                        <select class="form-control" id="client">
-                            <option>All Clients</option>
-                            <option>client 1</option>
-                            <option>client 2</option>
-                        </select>
-                    </div>
-                    <div class="col-sm-2">
-                        <select class="form-control" id="project">
-                            <option>All Projects</option>
-                            <option>project 1</option>
-                            <option>project 2</option>
-                        </select>
-                    </div>
-                    <div class="col-sm-2">
-                        <select class="form-control" id="priority">
-                            <option>All Priorities</option>
-                            <option>Priorities 1</option>
-                            <option>Priorities 2</option>
-                        </select>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav navbar-right ">
-                            <div class ="form-inline">
-                                <input type = "date">
-                                <input type = "date">
-                            </div>
-
-                        </ul>
-                    </div><!--/.nav-collapse -->
-                </div>
-            </div>
+            @yield('dashboardnav')
         </nav>
 
         @yield('content')
