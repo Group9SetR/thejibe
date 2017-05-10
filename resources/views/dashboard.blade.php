466805cee3a5fb4d1faf69aa77e5282220b7edd2
Hansol <leehansol1993@gmail.com>
2017-05-10 14:22:30 -0700

    Schedule things

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -61,32 +61,62 @@
             </tr>
             </thead>
             <tbody>
+
+            <!-- First row - Show User's profile and Scheduled bar -->
             <tr>
                 <th scope="row">
                     <div class = "row" id ="profile">
                         <div class="col-sm-2">
                             <img src="1.png" class="img-circle" alt="profile picture" width="50" height="50">
                         </div>
+                        <div id = propileName>
                         <div class="col-sm-5" id = "name">
-                            <P id = "name"><strong>Hansol Lee</strong></P>
+                            <P><strong>Hansol Lee</strong></P>
                         </div>
                         <div class="col-xs-1 pull-right">
                             <button type="button" class="btn btn-link">
                                 <span class="glyphicon glyphicon-chevron-down"></span>
                             </button>
                         </div>
+                        </div>
                     </div>
                 </th>
-                <div class = "scheduledBar">
-                <td colspan="10"> 65h/ 80h(81%)scheduled</td>
-                </div>
-
+                <td colspan="10" >
+                    <div id ="scheduledBar">
+                        <p id ="scheduledText">65h/ 80h(81%) scheduled</p>
+                    </div>
+                </td>
             </tr>
+            <!-- end First row -->
+
+            <!-- Next row - Show User's task and Schedule  -->
             <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                <th scope="row">
+                    <div class = "row" id = "task">
+                        <div>
+                            <div class = "pull-left">
+                                <p id = "taskName"><strong>Debug homepage for Sutton...</strong></p>
+                                <p id = "companyName">Sutton Reality</p>
+                            </div>
+                            <div class ="pull-right">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar"
+                             aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+                            40%
+                        </div>
+                    </div>
+                </th>
+                <!-- Top th which is date is reserved depends on project task change bg color -->
+                <td colspan="7" >
+                    <div id ="taskBar"></div>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr>
                 <th scope="row">3</th>
