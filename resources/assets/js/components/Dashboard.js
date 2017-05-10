cba873fc2e89fe9313cfd9fc481ea04e2c1a24e6
Vincent Lee <vincent.h.lee@outlook.com>
2017-05-10 14:05:09 -0700

    generate dashboard template in JSX instead. simple panels to display tasks.

--- resources/assets/js/components/Dashboard.js
+++ resources/assets/js/components/Dashboard.js
@@ -35,13 +35,22 @@ class Dashboard extends Component {
         return this.state.tasks.map(task => {
             return (
                 <tr key={task.id}>
-                    <td>{ task['project-name'] }</td>
-                    <td>{ task.id }</td>
-                    {
-                        task.completed?<td>Y</td> : <td>N</td>
-                    }
-                    <td>{ task.description }</td>
-                    <td>{ task['created-on'] }</td>
+                    <th scope="row">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                { task['project-name'] }
+                                {
+                                    (task.priority === "") ? <span></span>:
+                                        <span className="panel panel-primary" style={{ "float":"right"}}>{ task.priority }</span>
+                                }
+                            </div>
+                            <div className="panel-body">
+                                <p>{ task.description }</p><p>{ task['created-on'] }</p>
+                            </div>
+
+                        </div>
+                    </th>
+                    <td colSpan="10">TIMESPAN</td>
                 </tr>
             );
         })
@@ -50,20 +59,45 @@ class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h2>Tasks</h2>
-
-                <table className="table">
+                <table className="table table-bordered" id="task_table">
                     <thead>
-                    <tr>
-                        <th>Project</th>
-                        <th>ID</th>
-                        <th>Completed</th>
-                        <th>Description</th>
-                        <th>Created</th>
-                    </tr>
+                        <tr>
+                            <th rowSpan="2" style={{"width": "12%"}}></th>
+                            <th className="text-center" colSpan="5"> 9-13 Jan</th>
+                            <th className="text-center" colSpan="5"> 16-20 Jan</th>
+                        </tr>
+                        <tr>
+                            <th>9</th>
+                            <th>10</th>
+                            <th>11</th>
+                            <th>12</th>
+                            <th>13</th>
+                            <th>16</th>
+                            <th>17</th>
+                            <th>18</th>
+                            <th>19</th>
+                            <th>20</th>
+                        </tr>
                     </thead>
-
                     <tbody>
+                    <tr>
+                        <th scope="row">
+                            <div className = "row" id ="profile">
+                                <div className="col-sm-2">
+                                    <img src="1.png" className="img-circle" alt="profile picture" width="50" height="50"/>
+                                </div>
+                                <div className="col-sm-5" id = "name">
+                                    <p id = "name"><strong>Hansol Lee</strong></p>
+                                </div>
+                                <div className="col-xs-1 pull-right">
+                                    <button type="button" className="btn btn-link">
+                                        <span className="glyphicon glyphicon-chevron-down"> </span>
+                                    </button>
+                                </div>
+                            </div>
+                        </th>
+                        <td colSpan="10"> 65h/ 80h(81%)scheduled</td>
+                    </tr>
                     { this.renderTasks() }
                     </tbody>
                 </table>
