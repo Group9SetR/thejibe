13e1cd238e39bf457c5204e5057ef733f35832c3
Vincent Lee <vincent.h.lee@outlook.com>
2017-05-16 13:00:57 -0700

    extracted and refactored loader widget from layout to dashboard exclusively. refactored layout.app for more modularization

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,28 @@
 @extends('layouts.app')
+@section('title')
+    <title>Dashboard - {{ config('app.name', 'Laravel') }}</title>
+@endsection
+@section('css')
+    <link href="{{asset('css/dashboard.css')}}" rel="stylesheet">
+    <link href="{{asset('css/timer.css')}}" rel="stylesheet">
+@endsection
+@section('script')
+    <script>
+        document.ready=startTimer();
+        var myVar;
+
+        function startTimer() {
+            myVar = setTimeout(showPage, 3000);
+        }
+
+        function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("app").style.display = "block";
+        }
+    </script>
+@endsection
+
+
 @section('content')
     <div id="dashboard"></div>
 @endsection
