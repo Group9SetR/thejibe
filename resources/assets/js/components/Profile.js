ae588455b1160f54e2da49cfa9132a3f43bcfda8
Daniel Capacio <danielcapacio@gmail.com>
2017-05-16 16:12:55 -0700

    Availability on profile view

--- resources/assets/js/components/Profile.js
+++ resources/assets/js/components/Profile.js
@@ -34,7 +34,11 @@ class Profile extends Component {
 class Content extends Component {
     constructor(props){
         super(props);
+        this.handleAvailabilityForm = this.handleAvailabilityForm.bind(this);
+        this.handleHolidaysForm = this.handleHolidaysForm.bind(this);
     }
+    handleAvailabilityForm() {}
+    handleHolidaysForm() {}
     render() {
         var current = this.props.current || 'Schedule';
         return(
@@ -42,7 +46,76 @@ class Content extends Component {
                 <div className="panel-heading">
                     {current}
                 </div>
-                <div className="panel-body"></div>
+                <div className="panel-body">
+                    <form onSubmit={this.handleAvailabilityForm}>
+                        <h4>Availability</h4>
+                        <div className="form-group">
+                            <label>Date effective:&nbsp;</label>
+                            <input type="date" name="week"/>
+                        </div>
+                        <div className="form-group">
+                            <table className= "table table-striped table-bordered table-hover table-condensed">
+                                <tr className="info">
+                                    <th>Mon</th><th>Tues</th><th>Wed</th><th>Thurs</th><th>Fri</th>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" name="mon"/></td>
+                                    <td><input type="checkbox" name="tues"/></td>
+                                    <td><input type="checkbox" name="wed"/></td>
+                                    <td><input type="checkbox" name="thurs"/></td>
+                                    <td><input type="checkbox" name="fri"/></td>
+                                </tr>
+                            </table>
+                        </div>
+                        <input className="btn btn-primary" type="submit" value="Save"/>
+                    </form>
+                    <form onSubmit={this.handleHolidaysForm}>
+                        <h4>Holidays</h4>
+                        <div className="form-group">
+                            <table className= "table table-striped table-bordered table-hover table-condensed">
+                                <tr>
+                                    <th>2017</th>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" name=""/>&nbsp;New Year's Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Family Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Good Friday</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Victoria Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Canada Day</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" name=""/>&nbsp;B.C. Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Labour Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Thanksgiving Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Remembrance Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Christmas Day</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="form-group">
+                            <table className= "table table-striped table-bordered table-hover table-condensed">
+                                <tr>
+                                    <th>2018</th>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" name=""/>&nbsp;New Year's Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Family Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Good Friday</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Victoria Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Canada Day</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="checkbox" name=""/>&nbsp;B.C. Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Labour Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Thanksgiving Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Remembrance Day</td>
+                                    <td><input type="checkbox" name=""/>&nbsp;Christmas Day</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <input className="btn btn-primary" type="submit" value="Save"/>
+                    </form>
+                </div>
             </div>
         );
     }
