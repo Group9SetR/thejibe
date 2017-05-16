6b0e0598436a9a86288e35216d18777247b54c8c
Vincent Lee <vincent.h.lee@outlook.com>
2017-05-16 16:10:39 -0700

    TIMER IMPORTED. HOLLA  AT MY MODULARIZATION.

--- resources/assets/js/components/RefactorDashboard.js
+++ resources/assets/js/components/RefactorDashboard.js
@@ -9,15 +9,16 @@ class RefactorDashboard extends Component {
         this.state = {
             tasks: [],
             currentprofile: [],
-            calendar: []
+            calendar: [],
+            currenttimer: []
         }
-        this.timer = "";
         this.state.calendar = new Calendar();
         this.state.calendar.init();
         this.taskList = this.taskList.bind(this);
         this.currentProfile  = this.currentProfile.bind(this);
         this.header = this.header.bind(this);
         this.handleFilter = this.handleFilter.bind(this);
+        this.handleTimer = this.handleTimer.bind(this);
     }
 
     componentDidMount() {
@@ -85,6 +86,10 @@ class RefactorDashboard extends Component {
         }
     }
 
+    handleTimer(task) {
+        this.setState({currenttimer:task});
+    }
+
     render() {
         return (
             <div>
@@ -101,9 +106,10 @@ class RefactorDashboard extends Component {
                         <Tasks
                             calendar={this.state.calendar}
                             tasks={this.state.tasks}
-                            onTimerChange={this.manageTimer}/>
-                        <Timer current={this.timer}/>
+                            onTimerChange={this.handleTimer}/>
                     </table>
+
+                    <Timer timer={this.state.currenttimer}/>
                 </div>
             </div>
         );
@@ -133,7 +139,6 @@ class ColumnHeader extends Component {
         );
     }
 }
-
 
 class TableHeader extends Component {
     constructor(props) {
@@ -254,8 +259,15 @@ class FilterBar extends Component {
 }
 
 class Tasks extends Component {
+
     constructor(props) {
         super(props);
+        this.startTimer = this.startTimer.bind(this);
+    }
+
+    startTimer(e) {
+        var timer = {"id":e.target.dataset.taskId, "content":e.target.dataset.taskDesc};
+        this.props.onTimerChange(timer);
     }
 
     render() {
@@ -358,8 +370,10 @@ class Tasks extends Component {
                                     </div>
                                     <div className ="col-sm-3" style={{ "float":"right"}}>
 
-                                        <button type="button" onclick={} data-task-id={task.id} className="btn btn-default btn-sm pull-right" >
-                                            <span className="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+                                        <button type="button" onClick={this.startTimer}
+                                                data-task-id={task.id} data-task-desc={task.content}
+                                                className="btn btn-default btn-sm pull-right" >
+                                            <span className="glyphicon glyphicon glyphicon-time timer-btn" aria-hidden="true"></span>
                                         </button>
                                     </div>
                                 </div>
@@ -376,16 +390,97 @@ class Tasks extends Component {
 }
 
 class Timer extends Component {
+
     constructor(props) {
         super(props);
+        this.state={
+            current: [],
+            seconds: 0
+        };
+        this.timer = null;
+        this.log_time = this.log_time.bind(this);
+        this.handle_logTimeSubmit = this.handle_logTimeSubmit.bind(this);
+        this.get_hours = this.get_hours.bind(this);
+        this.get_minutes = this.get_minutes.bind(this);
+        this.get_seconds = this.get_seconds.bind(this);
+        this.handle_start = this.handle_start.bind(this);
+        this.handle_pause = this.handle_pause.bind(this);
+        this.handle_clear = this.handle_clear.bind(this);
+        this.handle_descChange = this.handle_descChange.bind(this);
+        this.handle_logTimeSubmit = this.handle_logTimeSubmit.bind(this);
     }
+    log_time() {
+        var key = "twp_29i8q9BH4BGyLykU4jSMZVkj1OnI";
+        var base64 = new Buffer(key + ":xxx").toString("base64");
+        var date = new Date();
+        fetch('https://thejibe.teamwork.com/tasks/7576391/time_entries.json', {
+            method: 'POST',
+            headers: {
+                'Authorization': "BASIC " + base64,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "time-entry": {
+                    "description": "Testing Dates",
+                    "person-id": "173892",
+                    "date": date.getFullYear() + ("0" + (date.getMonth() + 1)).slice(-2) + ("0" + date.getDate()).slice(-2),
+                    "time": date.getHours() + ":" + date.getMinutes(),
+                    "hours": "2",
+                    "minutes": "00",
+                    "isbillable": "1"
+                }
+            })
+        })
+    }
+
+    get_hours() {
+        let hours = Math.floor(this.state.seconds / 3600)
+        return ("0" + hours).slice(-2);
+    }
+
+    get_minutes() {
+        let minutes = Math.floor(this.state.seconds / 60)
+        return ("0" + minutes).slice(-2);
+    }
+    get_seconds() {
+        let seconds = Math.floor(this.state.seconds % 60)
+        return ("0" + seconds).slice(-2);
+    }
+
+    handle_start() {
+        this.timer = setInterval( () =>
+                this.setState({
+                    seconds: this.state.seconds + 1
+                })
+            , 1000);
+    }
+
+    handle_pause() {
+        clearInterval(this.timer);
+    }
+
+    handle_clear() {
+        clearInterval(this.timer);
+        this.setState({
+            seconds: 0
+        });
+    }
+
+    handle_descChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handle_logTimeSubmit() {
+        // send to teamwork?
+    }
+
     render() {
-        if(this.props.timer == "") {
+        if(Array.isArray(this.props.timer) && !this.props.timer.length) {
             return (<div></div>);
         }
         var current = this.props.timer;
         return (
-            <div className="logtimer" style={{visibility: 'hidden'}} value={current.id}>
+            <div className="logtimer" style={{visibility: 'visible'}} value={current.id}>
                 <div className="panel-group" id="accordion">
                     <div className="panel panel-default">
                         <div className="panel-heading" style={{"height":"50px"}}>
